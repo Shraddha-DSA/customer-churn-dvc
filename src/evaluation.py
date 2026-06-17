cca2dc1740bffffaf053e340d6df00acdbda2993
Shraddha Tiwari <shraddhatiwari345@gmail.com>
2026-06-17 15:24:10 +0530

    Added MLflow experiment tracking

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,18 +1,33 @@
 import json
 import joblib
 import pandas as pd
-from sklearn.metrics import accuracy_score
+import mlflow
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 test = pd.read_csv("data/processed/test.csv")
 X_test = test.drop("Churn",axis=1)
 y_test = test["Churn"]
 model = joblib.load("models/rf_model.pkl")
 pred = model.predict(X_test)
 acc = accuracy_score(y_test,pred)
+precision = precision_score(y_test,pred)
+recall = recall_score(y_test,pred)
+f1 = f1_score(y_test,pred)
 metrics = {
-    "accuracy":float(acc)
+    "accuracy":float(acc),
+    "precision":float(precision),
+    "recall":float(recall),
+    "f1_score":float(f1)
 }
 with open(
     "reports/metrics.json",
     "w"
 ) as f:
-    json.dump(metrics,f)
+    json.dump(metrics,f,indent=4)
+mlflow.set_experiment("Customer Churn Prediction")
+with mlflow.start_run():
+    mlflow.log_metric("accuracy",acc)
+    mlflow.log_metric('precision',precision)
+    mlflow.log_metric("recall",recall)
+    mlflow.log_metric("f1_score",f1)
+print(metrics)
+print("Evaluation donee!!")
--- src/model_training.py
+++ src/model_training.py
@@ -1,17 +1,34 @@
 import pandas as pd
 import yaml
 import joblib
+import os
+import mlflow
+import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
+os.makedirs("models", exist_ok=True)
 train = pd.read_csv("data/processed/train.csv")
 X=train.drop("Churn",axis=1)
 y=train["Churn"]
-params=yaml.safe_load(
-    open("params.yaml")
-)
-model = RandomForestClassifier(
-    n_estimators=params["n_estimators"],
-    max_depth=params["max_depth"],
-    random_state=42
-)
-model.fit(X,y)
-joblib.dump(model,"models/rf_model.pkl")
+
+with open("params.yaml","r") as f:
+    params = yaml.safe_load(f)
+n_estimators = params["n_estimators"]
+max_depth = params["max_depth"]
+mlflow.set_experiment("Customer Churn Prediction")
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=42
+    )
+    model.fit(X,y)
+    mlflow.log_param("n_estimators",n_estimators)
+    mlflow.log_param("max_depth",max_depth)
+    model_path = "models/rf_model.pkl"
+    joblib.dump(model,model_path)
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+    print(f"Model save {model_path}")
+print("Trained!")
